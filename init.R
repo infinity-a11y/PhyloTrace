fa09a9eaa76adff2541224d9733887e3d245cb78
infinity-a11y <115372379+infinity-a11y@users.noreply.github.com>
2024-01-29 15:24:42 +0100

    Update init.R

--- init.R
+++ init.R
@@ -1,8 +1,11 @@
 # Install and load renv (if not already installed)
-if (!requireNamespace("renv", quietly = TRUE)) {
-  install.packages("renv", lib = "~/R-4.3.2/library")
+if (!require("renv", quietly = TRUE, lib.loc = "~/R-4.3.2/library")) {
+  install.packages("renv", lib = "~/R-4.3.2/library", dependencies=TRUE,
+                   repos = "https://cloud.r-project.org/")
 }
 
+library(renv, lib.loc = "~/R-4.3.2/library")
+
 # Initialize and restore the renv environment
-renv::init()
+renv::init(force = T)
 renv::restore()
