5ebea448d39ce5f62d71feabfe645c21fb60eb0f
Marian Freisleben <115372379+infinity-a11y@users.noreply.github.com>
2024-02-28 21:58:53 +0100

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('newsletter-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    var email = document.getElementById('email').value;
-    // Here you would send the email to your server or a service like Mailchimp
-    alert('Thank you for subscribing!');
-});
