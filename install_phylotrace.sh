06021fdf98437e03422738553c5694e9cab29e7e
fpaskali <filip.paskali@gmail.com>
2024-03-17 14:53:31 +0100

    Reworked the installer

---
+++ install_phylotrace.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+CONDA_PATH=$( conda info --base )/bin/conda
+
+# Generate PhyloTrace Desktop Entry
+cat > PhyloTrace.desktop << EOF
+[Desktop Entry]
+Name=PhyloTrace
+Exec=$SCRIPT_DIR/run_phylotrace.sh
+Icon=PhyloTrace
+Terminal=true
+Type=Application
+Categories=Education
+EOF
+
+# Generate PhyloTrace run script
+cat > run_phylotrace.sh << EOF
+#!/bin/bash
+SCRIPT_DIR=$SCRIPT_DIR
+CONDA_PATH=$CONDA_PATH
+EOF
+
+cat >> run_phylotrace.sh << 'EOF'
+# Activate conda
+eval "$($CONDA_PATH shell.bash hook)"
+conda activate PhyloTrace
+if [ "$R_BROWSER" == "" ]; then
+   R_BROWSER=xdg-open Rscript -e "shiny::runApp('${SCRIPT_DIR}/App.R', launch.browser=TRUE)"
+else
+   Rscript -e "shiny::runApp('${SCRIPT_DIR}/App.R', launch.browser=TRUE)"
+fi
+EOF
+
+# Setting up the Desktop Icon
+mkdir -p $HOME/.local/share/applications
+mkdir -p $HOME/.local/share/icons/hicolor/scalable/apps
+mv $SCRIPT_DIR/PhyloTrace.desktop $HOME/.local/share/applications
+cp $SCRIPT_DIR/www/phylo.png $HOME/.local/share/icons/hicolor/scalable/apps/PhyloTrace.png
+chmod +x $SCRIPT_DIR/run_phylotrace.sh
--- start_phylotrace.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Get the script directory
-SCRIPT_DIR=$(dirname $BASH_SOURCE)
-
-# Activate conda
-eval "$(conda shell.bash hook)"
-conda activate PhyloTrace
-R_BROWSER=xdg-open Rscript -e "shiny::runApp('${SCRIPT_DIR}/App.R', launch.browser=TRUE)"
