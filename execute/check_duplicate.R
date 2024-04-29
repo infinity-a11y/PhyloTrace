47286ecc92d14391b81de3243000f2340edc9497
Marian Freisleben <115372379+infinity-a11y@users.noreply.github.com>
2024-04-29 20:17:55 +0200

    Changed to more suitable variable names

--- execute/check_duplicate.R
+++ execute/check_duplicate.R
@@ -6,20 +6,20 @@ assembly <- typing_meta$genome
 
 lines <- readLines(assembly)
 
-first <- str_extract(lines[seq(1, length(lines), by = 3)], "^[^\\s]+")
+names <- str_extract(lines[seq(1, length(lines), by = 3)], "^[^\\s]+")
 
 # Test if there are duplicates
-if(length(first) != length(unique(first))){
+if(length(names) != length(unique(names))){
   # add a number to the duplicates
-  for(i in 1:length(first)){
-    if(sum(first == first[i]) > 1){
-      first[i] <- paste0(first[i], "_", which(first == first[i]))
+  for(i in 1:length(names)){
+    if(sum(names == names[i]) > 1){
+      names[i] <- paste0(names[i], "_", which(names == names[i]))
     }
   }
   
   # substitute the respective lines in the file with the new names 
-  for(i in 1:length(first)){
-    lines[3*i - 2] <- paste0(first[i])
+  for(i in 1:length(names)){
+    lines[3*i - 2] <- paste0(names[i])
   }
   
   # save the new assembly to working directory
