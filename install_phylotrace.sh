281a84211a339f7519092b23464f0f4f57901296
fpaskali <filip.paskali@gmail.com>
2024-12-20 17:54:25 +0100

    Changed installation process

--- install_phylotrace.sh
+++ install_phylotrace.sh
@@ -5,6 +5,15 @@ CONDA_PATH=$( conda info --base )/bin/conda
 eval "$($CONDA_PATH shell.bash hook)"
 conda activate PhyloTrace
 
+#!/bin/bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+CONDA_PATH=$( conda info --base )/bin/conda
+
+eval "$($CONDA_PATH shell.bash hook)"
+conda activate PhyloTrace
+
+Rscript -e "source('required_packages.R')"
+
 # Generate PhyloTrace Desktop Entry
 cat > PhyloTrace.desktop << EOF
 [Desktop Entry]
@@ -36,8 +45,43 @@ else
 fi
 EOF
 
-# Install visNetwork modification
-Rscript -e "remotes::install_github('fpaskali/visNetwork', force = TRUE)"
+# Setting up the Desktop Icon
+mkdir -p $HOME/.local/share/applications
+mkdir -p $HOME/.local/share/icons/hicolor/scalable/apps
+mv $SCRIPT_DIR/PhyloTrace.desktop $HOME/.local/share/applications
+cp $SCRIPT_DIR/www/phylo.png $HOME/.local/share/icons/hicolor/scalable/apps/PhyloTrace.png
+chmod 700 $SCRIPT_DIR/run_phylotrace.sh
+
+# Generate PhyloTrace Desktop Entry
+cat > PhyloTrace.desktop << EOF
+[Desktop Entry]
+Name=PhyloTrace
+Exec=$SCRIPT_DIR/run_phylotrace.sh
+Icon=PhyloTrace
+Terminal=true
+Type=Application
+Categories=Education
+EOF
+
+# Generate PhyloTrace run script
+cat > run_phylotrace.sh << EOF
+#!/bin/bash
+SCRIPT_DIR=$SCRIPT_DIR
+CONDA_PATH=$CONDA_PATH
+EOF
+
+cat >> run_phylotrace.sh << 'EOF'
+# Activate conda
+eval "$($CONDA_PATH shell.bash hook)"
+conda activate PhyloTrace
+if [ "$R_BROWSER" != "" ]; then
+   Rscript -e "shiny::runApp('${SCRIPT_DIR}/App.R', launch.browser=TRUE)"
+elif [[ $(uname -r) == *"microsoft"* ]]; then
+   R_BROWSER=wslview Rscript -e "shiny::runApp('${SCRIPT_DIR}/App.R', launch.browser=TRUE)"
+else
+   R_BROWSER=xdg-open Rscript -e "shiny::runApp('${SCRIPT_DIR}/App.R', launch.browser=TRUE)"
+fi
+EOF
 
 # Setting up the Desktop Icon
 mkdir -p $HOME/.local/share/applications
@@ -45,3 +89,5 @@ mkdir -p $HOME/.local/share/icons/hicolor/scalable/apps
 mv $SCRIPT_DIR/PhyloTrace.desktop $HOME/.local/share/applications
 cp $SCRIPT_DIR/www/phylo.png $HOME/.local/share/icons/hicolor/scalable/apps/PhyloTrace.png
 chmod 700 $SCRIPT_DIR/run_phylotrace.sh
+
+echo "PhyloTrace successfuly installed!"
---
+++ required_packages.R
@@ -0,0 +1,82 @@
+# List of required packages
+required_cran_packages <- c(
+  "shiny",
+  "R.utils",
+  "igraph",
+  "shinyWidgets",
+  "shinydashboard",
+  "dashboardthemes",
+  "ggplot2",
+  "ggnewscale",
+  "ggplotify",
+  "grid",
+  "gridExtra",
+  "ape",
+  "tidyverse",
+  "rlang",
+  "tidytree",
+  "shinyFiles",
+  "dplyr",
+  "downloader",
+  "rvest",
+  "rmarkdown",
+  "knitr",
+  "kableExtra",
+  "fs",
+  "data.table",
+  "zoo",
+  "ggnetwork",
+  "rhandsontable",
+  "proxy",
+  "phangorn",
+  "cowplot",
+  "waiter",
+  "viridis",
+  "RColorBrewer",
+  "bslib",
+  "bsicons",
+  "DT",
+  "shinyBS",
+  "openssl",
+  "shinyjs",
+  "logr",
+  "BiocManager",
+  "remotes"
+)
+
+required_bioconductor_packages <- c(
+  "treeio",
+  "ggtree",
+  "ggtreeExtra",
+  "Biostrings",
+  "pwalign",
+  "ComplexHeatmap"
+)
+
+required_github_packages <- c(
+  "fpaskali/visNetwork"
+)
+
+# Install CRAN packages only if they are not already installed
+for (package in required_cran_packages) {
+  if (!require(package, character.only = TRUE)) {
+    install.packages(package)
+  }
+}
+
+# Install Bioconductor packages only if they are not already installed
+for (package in required_bioconductor_packages) {
+  if (!require(package, character.only = TRUE)) {
+    BiocManager::install(package)
+  }
+}
+
+# Install GitHub packages only if they are not already installed
+for (package_repo in required_github_packages) {
+  package <- strsplit(package_repo, "/")[[1]][2]
+  if (!require(package, character.only = TRUE)) {
+    remotes::install_github(package)
+  }
+}
+
+cat("All packages successfully installed!\n")
